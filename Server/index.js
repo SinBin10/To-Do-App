d4041e989ae69f1f926f7326a961213f9276d478
SinBin10 <bincode10@gmail.com>
2024-08-21 07:17:20 +0530

    handling request and response in the backend.

--- Server/index.js
+++ Server/index.js
@@ -1,11 +1,20 @@
 const express = require("express");
+const cors = require("cors");
 const taskModel = require("./models/task-model");
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("running fine..");
+});
+
+app.post("/create", async (req, res) => {
+  await taskModel.create({
+    task: req.body.task,
+  });
+  res.status(200).send();
 });
 
 app.listen(3000, () => {
